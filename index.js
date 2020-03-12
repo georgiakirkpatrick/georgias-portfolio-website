2b9a9c893153bca92d83216154d9c7ab4eec65ec
Georgia Kirkpatrick <georgia.kirkpatrick@gmail.com>
2020-03-11 19:29:09 -0700

    Add project content and modify css
    Make site responsive by using css, add project info to
    Projects section.  Add a menu.  Add overlays with additional
    info about projects.

---
+++ index.js
@@ -0,0 +1,53 @@
+function listenForChivoloClick() {
+    $('#chivolo-section').on('click', '#chivolo-button', function() {
+        let chivoloOverlay = document.getElementById("chivolo-overlay")
+
+        $(chivoloOverlay).addClass('overlay-on')
+
+        $('#close-chivolo').on('click', function() {
+    
+            $(chivoloOverlay).removeClass('overlay-on')
+        })
+    })
+}
+
+function listenForWearingClick() {
+    $('#wearing-section').on('click', '#wearing-button', function() {
+        let wearingOverlay = document.getElementById("wearing-overlay")
+
+        $(wearingOverlay).addClass('overlay-on')
+
+        $('#close-wearing').on('click', function() {
+    
+            $(wearingOverlay).removeClass('overlay-on')
+        })
+    })
+}
+
+function listenMenuItem() {
+    let menuItems = document.getElementById("menu-items")
+    let menuOverlay = document.getElementById("menu-overlay")
+
+    menuItems.addEventListener('click', function() {
+        $('#menu-icon').toggleClass('cross')
+        $(menuOverlay).toggleClass('overlay-on')
+    })
+}
+
+function listenForMenuClick() {
+    $('nav').on('click', '#menu-icon', function() {
+        $('#menu-icon').toggleClass('cross')
+
+        let menuOverlay = document.getElementById("menu-overlay")
+        $(menuOverlay).toggleClass('overlay-on')
+    })
+}
+
+function handlePortfolio() {
+    listenForMenuClick()
+    listenMenuItem()
+    listenForWearingClick()
+    listenForChivoloClick()
+}
+
+$(handlePortfolio)
